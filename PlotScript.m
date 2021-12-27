831a9ff29501e401da9fa86456c3fdf8442ea45e
Samet <1sametyakut@gmail.com>
2021-12-27 15:03:54 +0300

    Correction on figures and minor changes

--- PlotScript.m
+++ PlotScript.m
@@ -5,7 +5,7 @@ plot(out.InputVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([0 0.04]);
 grid on;
 grid minor;
-title('Input Voltage Waveform');
+title('Input Voltage Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% Output Voltage
@@ -14,7 +14,7 @@ plot(out.OutputVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Output Voltage Waveform (f_s = 3 kHz & D = 0.7)');
+title('Output Voltage Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% Output Current
@@ -23,7 +23,7 @@ plot(out.ArmatureCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Armature Current Waveform (f_s = 3 kHz & D = 0.7)');
+title('Armature Current Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
 %% IGBT Voltage
@@ -32,7 +32,7 @@ plot(out.IgbtVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('IGBT Voltage Waveform (f_s = 3 kHz & D = 0.7)');
+title('IGBT Voltage Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% IGBT Current
@@ -41,7 +41,7 @@ plot(out.IgbtCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('IGBT Current Waveform (f_s = 3 kHz & D = 0.7)');
+title('IGBT Current Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
 %% Diode Voltage
@@ -50,7 +50,7 @@ plot(out.DiodeVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Freewheeling Diode Voltage Waveform (f_s = 3 kHz & D = 0.7)');
+title('Freewheeling Diode Voltage Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% Diode Current
@@ -59,6 +59,6 @@ plot(out.DiodeCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Freewheeling Diode Current Waveform (f_s = 3 kHz & D = 0.7)');
+title('Freewheeling Diode Current Waveform (f_s = 3 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
