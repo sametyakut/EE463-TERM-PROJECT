6ac3d0d57ce33c5a3b835b304ad22769809fc8a8
Samet <1sametyakut@gmail.com>
2021-12-28 19:15:11 +0300

    Correction on simulation results

--- PlotScript.m
+++ PlotScript.m
@@ -5,7 +5,7 @@ plot(out.InputVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([0 0.04]);
 grid on;
 grid minor;
-title('Input Voltage Waveform (f_s = 3 kHz & D = 0.8)');
+title('Input Voltage Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% Output Voltage
@@ -14,7 +14,7 @@ plot(out.OutputVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Output Voltage Waveform (f_s = 3 kHz & D = 0.8)');
+title('Output Voltage Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% Output Current
@@ -23,7 +23,7 @@ plot(out.ArmatureCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([2 2.02]);
 grid on;
 grid minor;
-title('Armature Current Waveform (f_s = 3 kHz & D = 0.8)');
+title('Armature Current Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
 %% IGBT Voltage
@@ -32,7 +32,7 @@ plot(out.IgbtVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('IGBT Voltage Waveform (f_s = 3 kHz & D = 0.8)');
+title('IGBT Voltage Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% IGBT Current
@@ -41,7 +41,7 @@ plot(out.IgbtCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('IGBT Current Waveform (f_s = 3 kHz & D = 0.8)');
+title('IGBT Current Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
 %% Diode Voltage
@@ -50,7 +50,7 @@ plot(out.DiodeVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Freewheeling Diode Voltage Waveform (f_s = 3 kHz & D = 0.8)');
+title('Freewheeling Diode Voltage Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
 %% Diode Current
@@ -59,7 +59,7 @@ plot(out.DiodeCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.02]);
 grid on;
 grid minor;
-title('Freewheeling Diode Current Waveform (f_s = 3 kHz & D = 0.8)');
+title('Freewheeling Diode Current Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
 %% Input Current
@@ -68,7 +68,7 @@ plot(out.InputCurrent,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([2 2.04]);
 grid on;
 grid minor;
-title('Input Current Waveform (f_s = 3 kHz & D = 0.8)');
+title('Input Current Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Current (A)');
 xlabel('Time (s)');
 %% Output Voltage of Rectifier
@@ -77,6 +77,6 @@ plot(out.RectifierOutputVoltage,'LineWidth',1.5,'Color',[0.7 0 0]);
 xlim([1 1.04]);
 grid on;
 grid minor;
-title('Three Phase Full-Bridge Rectifier Output Voltage Waveform (f_s = 3 kHz & D = 0.8)');
+title('Three Phase Full-Bridge Rectifier Output Voltage Waveform (f_s = 5 kHz & D = 0.8)');
 ylabel('Voltage (V)');
 xlabel('Time (s)');
